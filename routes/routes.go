5421e6444a1365b880f49cc56375d7333fcd0afd
codefire53 <krinsaihsani@gmail.com>
2021-05-22 22:47:58 +0700

    [GREEN] Add tag endpoint routing

--- routes/routes.go
+++ routes/routes.go
@@ -14,16 +14,20 @@ type Route struct{}
 func (r *Route) Init() *mux.Router {
 	// init repositories
 	newsRepository := new(repositories.NewsRepository)
+	tagRepository := new(repositories.TagRepository)
 
 	// init services
 	newsService := services.InitNewsService(newsRepository)
+	tagService := services.InitTagService(tagRepository)
 
 	// init Controllers
 	newsController := controllers.InitNewsController(newsService)
+	tagController := controllers.InitTagController(tagService)
 
 	// init routes
 	router := mux.NewRouter().StrictSlash(false)
 	news := router.PathPrefix("/news").Subrouter()
+	tag := router.PathPrefix("/tag").Subrouter()
 
 	//news endpoint
 	news.HandleFunc("/", newsController.Create).Methods("POST")
@@ -31,6 +35,12 @@ func (r *Route) Init() *mux.Router {
 	news.HandleFunc("/{id}", newsController.Delete).Methods("DELETE")
 	news.HandleFunc("/{id}", newsController.GetDetail).Methods("GET")
 	news.HandleFunc("", newsController.List).Methods("GET")
+
+	//tag endpoint
+	tag.HandleFunc("/", tagController.Create).Methods("POST")
+	tag.HandleFunc("/{id}", tagController.Update).Methods("PUT")
+	tag.HandleFunc("/{id}", tagController.Delete).Methods("DELETE")
+	tag.HandleFunc("", tagController.List).Methods("GET")
 
 	return router
 }
