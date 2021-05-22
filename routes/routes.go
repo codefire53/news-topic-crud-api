920950b5c798d81b7f75a9c9e8f9338f2aa5571f
codefire53 <krinsaihsani@gmail.com>
2021-05-22 12:02:52 +0700

    [GREEN] Define news endpoint routes

--- routes/routes.go
+++ routes/routes.go
@@ -2,6 +2,9 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"news-topic-api/controllers"
+	"news-topic-api/repositories"
+	"news-topic-api/services"
 )
 
 // Route ...
@@ -9,7 +12,25 @@ type Route struct{}
 
 // Init is the initiator for the route location
 func (r *Route) Init() *mux.Router {
+	// init repositories
+	newsRepository := new(repositories.NewsRepository)
+
+	// init services
+	newsService := services.InitNewsService(newsRepository)
+
+	// init Controllers
+	newsController := controllers.InitNewsController(newsService)
+
 	// init routes
 	router := mux.NewRouter().StrictSlash(false)
+	news := router.PathPrefix("/news").Subrouter()
+
+	//news endpoint
+	news.HandleFunc("/", newsController.Create).Methods("POST")
+	news.HandleFunc("/{id}", newsController.Update).Methods("PUT")
+	news.HandleFunc("/{id}", newsController.Delete).Methods("DELETE")
+	news.HandleFunc("/{id}", newsController.GetDetail).Methods("GET")
+	news.HandleFunc("", newsController.List).Methods("GET")
+
 	return router
 }
